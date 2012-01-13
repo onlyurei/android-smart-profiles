09177fd1c61674ca1df69a31b5227be93b4924b0
Cheng Fan <fancheng33@gmail.com>
2012-01-12 23:01:10 -0600

    Remove hard-coded contents from ActivityHelp

--- SmartProfiles/src/com/intofan/android/smartprofiles/ActivityHelp.java
+++ SmartProfiles/src/com/intofan/android/smartprofiles/ActivityHelp.java
@@ -1,8 +1,6 @@
 package com.intofan.android.smartprofiles;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
 import android.webkit.WebChromeClient;
@@ -23,46 +21,7 @@ public class ActivityHelp extends Activity {
 				activity.setProgress(progress * 100);
 			}
 		});
-		webview.setWebViewClient(new WebViewClient() {
-			public void onReceivedError(WebView view, int errorCode,
-					String description, String failingUrl) {
-				UtilAlert.toast(getApplicationContext(), description, 1);
-			}
-
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				try {
-					if (url != null) {
-						if (url.startsWith("market://")
-								|| url.startsWith("http://")
-								|| url.startsWith("https://")) {
-							view.getContext().startActivity(
-									new Intent(Intent.ACTION_VIEW, Uri
-											.parse(url)));
-							return true;
-						} else if (url.startsWith("mailto:")) {
-							final Intent emailIntent = new Intent(
-									android.content.Intent.ACTION_SEND);
-							emailIntent.setType("plain/text");
-							emailIntent.putExtra(
-									android.content.Intent.EXTRA_EMAIL,
-									new String[] { "intofansoft@gmail.com" });
-							emailIntent.putExtra(
-									android.content.Intent.EXTRA_SUBJECT,
-									"Smarter Profiles Feedback");
-							startActivity(Intent.createChooser(emailIntent,
-									"Send E-mail.."));
-							return true;
-						}
-						return false;
-					}
-					return false;
-				} catch (Exception e) {
-					UtilAlert.alert(activity, getString(R.string.error),
-							e.toString());
-				}
-				return false;
-			}
-		});
-		webview.loadUrl("http://intofan.com/smarterprofiles/help.html");
+		webview.setWebViewClient(new WebViewClient() {});
+		webview.loadUrl(getString(R.string.help_url));
 	}
 }
